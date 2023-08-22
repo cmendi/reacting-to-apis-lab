ec0256308706aa3e744f208e98493bf89ca39ec4
mendi <mendenhall_chris1246@yahoo.com>
2023-08-22 15:32:39 -0700

    finished styling the page

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,35 @@ import React from "react";
 const App = () => {
 	return (
 		<>
-			<div>
-				<h1>Hello</h1>
+			<div className="container">
+				<div className="row justify-content-center">
+					<div className="col-md-4">
+						<div className="d-flex justify-content-center">
+							<img src="https://ghibliapi.vercel.app/images/logo.svg" className="border border-3 border-dark rounded m-3" alt="Studio Ghibli" />
+						</div>
+						<div className="d-flex justify-content-between">
+							<button className="btn btn-primary">Films</button>
+							<button className="btn btn-primary">People</button>
+						</div>
+					</div>
+					<div className="container d-flex justify-content-center m-5">
+						<div className="card shadow col-7">
+							<div className="card-body">
+								<h3 className="card-title text-center">Studio Ghibli API Lab</h3>
+								<p className="card-text">
+									This lab shows off my awesome React skills. It utilizes conditional rendering, fetching data from REST API utilizing an useEffect hook, managing mulitple arra maps utilizing an
+									useState hook, and Boostrap for that iconic style. Click a button to view the resources from Studio Ghibli and enjoy!
+								</p>
+								<h6 className=" text-center card-subtitle m-2 text-body-secondary">
+									If you like what you see, contact me up here:{" "}
+									<a href="https://mendenhallmedias.com/" target="_blank" className="text-primary">
+										My Website
+									</a>
+								</h6>
+							</div>
+						</div>
+					</div>
+				</div>
 			</div>
 		</>
 	);
