12f488c071c8492209426f29d1dd23d605b99ec7
mendi <mendenhall_chris1246@yahoo.com>
2023-08-22 15:42:18 -0700

    fixed up styling

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,11 @@ const App = () => {
 		<>
 			<div className="container">
 				<div className="row justify-content-center">
-					<div className="col-md-4">
-						<div className="d-flex justify-content-center">
+					<div className="col-md-6">
+						<div className="row justify-content-center">
 							<img src="https://ghibliapi.vercel.app/images/logo.svg" className="border border-3 border-dark rounded m-3" alt="Studio Ghibli" />
 						</div>
-						<div className="d-flex justify-content-between">
+						<div className="d-flex justify-content-center justify-content-evenly">
 							<button className="btn btn-primary">Films</button>
 							<button className="btn btn-primary">People</button>
 						</div>
@@ -19,7 +19,7 @@ const App = () => {
 							<div className="card-body">
 								<h3 className="card-title text-center">Studio Ghibli API Lab</h3>
 								<p className="card-text">
-									This lab shows off my awesome React skills. It utilizes conditional rendering, fetching data from REST API utilizing an useEffect hook, managing mulitple arra maps utilizing an
+									This lab shows off my awesome React skills. It utilizes conditional rendering, fetching data from REST API utilizing an useEffect hook, managing mulitple array maps utilizing an
 									useState hook, and Boostrap for that iconic style. Click a button to view the resources from Studio Ghibli and enjoy!
 								</p>
 								<h6 className=" text-center card-subtitle m-2 text-body-secondary">
